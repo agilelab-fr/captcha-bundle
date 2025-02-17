a71ea8c4942c20c18de8e88777ef497170dca78a
Agilelab.fr <contact@agilelab.fr>
2025-02-17 13:15:18 +0100

    Adding config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,11 +15,11 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-            ->integerNode('width')->defaultValue(120)->end()
-            ->integerNode('height')->defaultValue(40)->end()
-            ->integerNode('length')->defaultValue(6)->end()
-            ->integerNode('lines')->defaultValue(8)->end()
-            ->scalarNode('characters')->defaultValue('ABCDEFGHJKLMNPQRSTUVWXYZ23456789')->end()
+                ->integerNode('width')->end()
+                ->integerNode('height')->end()
+                ->integerNode('length')->end()
+                ->integerNode('lines')->end()
+                ->scalarNode('characters')->end()
             ->end();
 
         return $treeBuilder;
