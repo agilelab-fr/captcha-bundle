cd5def15fbdb73c4c905c477120efb1ab4f1b276
Agilelab.fr <contact@agilelab.fr>
2025-02-17 10:46:17 +0100

    Adding config

--- src/AgilelabFrCaptchaBundle.php
+++ src/AgilelabFrCaptchaBundle.php
@@ -4,9 +4,9 @@ namespace AgilelabFr\CaptchaBundle;
 
 use AgilelabFr\CaptchaBundle\DependencyInjection\CaptchaBundleExtension;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AgilelabFrCaptchaBundle extends AbstractBundle
+class AgilelabFrCaptchaBundle extends Bundle
 {
     public function getPath(): string
     {
