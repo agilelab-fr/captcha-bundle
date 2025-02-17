579957f9abcd9b46f37bbb5c210438f5debdef7e
Agilelab.fr <contact@agilelab.fr>
2025-02-17 13:40:42 +0100

    Adding config

--- src/DependencyInjection/CaptchaBundleExtension.php
+++ src/DependencyInjection/CaptchaBundleExtension.php
@@ -25,6 +25,5 @@ class CaptchaBundleExtension extends Extension
         $container->setParameter('captcha_bundle.height', $config['height']);
         $container->setParameter('captcha_bundle.length', $config['length']);
         $container->setParameter('captcha_bundle.lines', $config['lines']);
-        $container->setParameter('captcha_bundle.characters', $config['characters']);
     }
 }
--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -7,21 +7,20 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-//    public function getConfigTreeBuilder(): TreeBuilder
-//    {
-//        $treeBuilder = new TreeBuilder('captcha_bundle');
-//
-//        $rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('captcha_bundle');
-//
-//        $rootNode
-//            ->children()
-//                ->integerNode('width')->end()
-//                ->integerNode('height')->end()
-//                ->integerNode('length')->end()
-//                ->integerNode('lines')->end()
-//                ->scalarNode('characters')->end()
-//            ->end();
-//
-//        return $treeBuilder;
-//    }
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('captcha_bundle');
+
+        $rootNode = method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('captcha_bundle');
+
+        $rootNode
+            ->children()
+                ->integerNode('width')->end()
+                ->integerNode('height')->end()
+                ->integerNode('length')->end()
+                ->integerNode('lines')->end()
+            ->end();
+
+        return $treeBuilder;
+    }
 }
