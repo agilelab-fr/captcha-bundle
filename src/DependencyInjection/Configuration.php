97e02d95ccca6100b44c8614d73f7f2efe990f64
Agilelab.fr <contact@agilelab.fr>
2025-02-19 09:07:19 +0100

    Workflow

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace AgilelabFr\CaptchaBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -15,11 +16,11 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-                ->integerNode('width')->defaultValue(120)->end()
-                ->integerNode('height')->defaultValue(40)->end()
-                ->integerNode('length')->defaultValue(6)->end()
-                ->integerNode('lines')->defaultValue(8)->end()
-                ->scalarNode('characters')->defaultValue("ABCDEFGHJKLMNPQRSTUVWXYZ23456789")->end()
+            ->integerNode('width')->defaultValue(120)->end()
+            ->integerNode('height')->defaultValue(40)->end()
+            ->integerNode('length')->defaultValue(6)->end()
+            ->integerNode('lines')->defaultValue(8)->end()
+            ->scalarNode('characters')->defaultValue("ABCDEFGHJKLMNPQRSTUVWXYZ23456789")->end()
             ->end();
 
         return $treeBuilder;
--- src/Services/CaptchaService.php
+++ src/Services/CaptchaService.php
@@ -4,28 +4,60 @@ namespace AgilelabFr\CaptchaBundle\Services;
 
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CaptchaService
 {
     private ParameterBagInterface $params;
+    private ValidatorInterface $validator;
 
-    public function __construct(ParameterBagInterface $params)
+    public function __construct(ParameterBagInterface $params, ValidatorInterface $validator)
     {
         $this->params = $params;
+        $this->validator = $validator;
+    }
+
+    public function validateConfig(array $config): void
+    {
+        $constraints = new Collection([
+            'width' => [new NotBlank(), new Positive()],
+            'height' => [new NotBlank(), new Positive()],
+            'length' => [new NotBlank(), new Positive()],
+            'lines' => [new NotBlank(), new Positive()],
+            'characters' => [new NotBlank(), new Length(['min' => 6])]
+        ]);
+
+        $errors = $this->validator->validate($config, $constraints);
+        if (count($errors) > 0) {
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[] = $error->getMessage();
+            }
+            throw new ValidationFailedException(implode(', ', $errorMessages), $errors);
+        }
+
     }
 
     public function generateCaptcha(Request $request): string
     {
-        $width = $this->params->get('captcha_bundle.width') ?? 120;
-        $height = $this->params->get('captcha_bundle.height') ?? 40;
-        $length = $this->params->get('captcha_bundle.length') ?? 6;
-        $lines = $this->params->get('captcha_bundle.lines') ?? 8;
-        $characters = $this->params->get('captcha_bundle.characters') ?? 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+
+        $config['width'] = (int)$this->params->get('captcha_bundle.width') ?? 120;
+        $config['height'] = (int)$this->params->get('captcha_bundle.height') ?? 40;
+        $config['length'] = (int)$this->params->get('captcha_bundle.length') ?? 6;
+        $config['lines'] = (int)$this->params->get('captcha_bundle.lines') ?? 8;
+        $config['characters'] = $this->params->get('captcha_bundle.characters') ?? 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+
+        $this->validateConfig($config);
 
         // Generate random CAPTCHA text
         $captchaText = '';
-        for ($i = 0; $i < $length; $i++) {
-            $captchaText .= $characters[rand(0, strlen($characters) - 1)];
+        for ($i = 0; $i < $config['length']; $i++) {
+            $captchaText .= $config['characters'][rand(0, strlen($config['characters']) - 1)];
         }
 
         // Store CAPTCHA text in session
@@ -33,7 +65,7 @@ class CaptchaService
         $session->set('captcha', $captchaText);
 
         // Create original CAPTCHA image
-        $image = imagecreatetruecolor($width, $height);
+        $image = imagecreatetruecolor($config['width'], $config['height']);
         $bgColor = imagecolorallocate($image, 230, 230, 230);
         $textColor = imagecolorallocate($image, 20, 40, 100);
         $lineColor = imagecolorallocate($image, 100, 120, 180);
@@ -42,14 +74,14 @@ class CaptchaService
         imagefill($image, 0, 0, $bgColor);
 
         // Add random lines
-        for ($i = 0; $i < $lines; $i++) {
+        for ($i = 0; $i < $config['lines']; $i++) {
             imagesetthickness($image, rand(1, 3));
-            imageline($image, rand(0, $width), rand(0, $height), rand(0, $width), rand(0, $height), $lineColor);
+            imageline($image, rand(0, $config['width']), rand(0, $config['height']), rand(0, $config['width']), rand(0, $config['height']), $lineColor);
         }
 
         // Add random dots
         for ($i = 0; $i < 100; $i++) {
-            imagesetpixel($image, rand(0, $width), rand(0, $height), $lineColor);
+            imagesetpixel($image, rand(0, $config['width']), rand(0, $config['height']), $lineColor);
         }
 
         // Add CAPTCHA text
